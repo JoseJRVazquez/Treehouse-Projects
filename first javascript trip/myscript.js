fc28976f9f35196b8c0595a4fab75d576a3c8cc9
jr vazquez <kwikteck@gmail.com>
2014-02-17 18:22:23 -0600

    Testing Comments

--- first javascript trip/myscript.js
+++ first javascript trip/myscript.js
@@ -1,10 +1,13 @@
+//This is a simple javascript program for practice with tree house lessons
+
+//Do a couple of console.logs
 console.log("Hello from myscript.js");
-console.log("Hello Again");
+console.log("Hello Again"); //This is not needed
 
+/* This line sets the initial variable name
 var name = prompt("What is your name?");
-
 alert("Hello " + name);
 
 name = "Nick";
-
 console.log("The user's name is " + name);
+*/
